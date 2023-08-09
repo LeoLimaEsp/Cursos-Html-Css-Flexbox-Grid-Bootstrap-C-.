82792ffed6aaeb6cb421410ba21727915c98a5eb
leonel <leonel.limae@hotmail.com>
2023-08-09 16:27:20 -0600

    Update

--- compilaciones_c#_vs/MVC_Escuela/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_Escuela.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
